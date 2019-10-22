5919f5860008cd36c6c67e8623c6a40d4d9a7d99
Victor <victorgibim@gmail.com>
2019-10-22 17:15:26 -0300

    at

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -33,27 +33,6 @@ const validationSchema = yup.object().shape({
             return true;
         }
     ),
-    email: yup
-    .string()
-    .label('E-mail')
-    .email()
-    .required()
-    .test('test-name', 'Enter Valid Email',
-    function(value) {
-        const emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        let isValidEmail = emailRegex.test(value);
-        if (!isValidEmail) {
-            return false;
-        }
-        return true;
-    }
-    ),
-    password: yup
-    .string()
-    .label('Senha')
-    .required()
-    .min(6, 'A senha tem que ter mais de 6 Caracteres')
-    .max(12, 'A senha deve ter até 12 caracteres'),
     terms: yup
     .boolean()
     .oneOf([true], 'Teste'),
@@ -97,6 +76,141 @@ const validationSchema = yup.object().shape({
             return true;
         }
     ),
+    cep: yup
+    .string()
+    .required()
+    .label('CEP')
+    .test(
+        'test-name', 'Nome não Pode conter Número',
+        function(value) {
+            const nameRegex = /^([0-9_\.\-])+(([0-9\-])+\.)+([0-9]{3,6})+$/;
+            let isValidName = nameRegex.test(value);
+            if(!isValidName) {
+                return false;
+            }
+            return true;
+        }
+    ),
+    email: yup
+    .string()
+    .label('E-mail')
+    .email()
+    .required()
+    .test('test-name', 'Enter Valid Email',
+    function(value) {
+        const emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        let isValidEmail = emailRegex.test(value);
+        if (!isValidEmail) {
+            return false;
+        }
+        return true;
+    }
+    ),
+    password: yup
+    .string()
+    .label('Senha')
+    .required()
+    .min(6, 'A senha tem que ter mais de 6 Caracteres')
+    .max(12, 'A senha deve ter até 12 caracteres'),
+    confirmPassword: yup
+    .string()
+    .required()
+    .label('Confirmar Senha')
+    .test('passwords-match', 'Passwords must match ya fool', function(value){
+        return this.parent.password === value;
+    }),
+    logradouro: yup
+    .string()
+    .label('Logradouro')
+    .required(),
+    numero: yup
+    .number()
+    .label('Numero')
+    .required(),
+    telefone1: yup
+    .number()
+    .label('Telefone 1')
+    .required(),
+    telefone2: yup
+    .number()
+    .label('Telefone 2')
+    .required(),
+    complemento: yup
+    .string()
+    .label('Complemento')
+    .required(),
+    uf: yup
+    .string()
+    .label('UF')
+    .max(2)
+    .test(
+        'test-name', 'UF não Pode conter Número',
+        function(value) {
+            const nameRegex = /^([a-z A-Z])+$/;
+            let isValidName = nameRegex.test(value);
+            if(!isValidName) {
+                return false;
+            }
+            return true;
+        }),
+    bairro: yup
+    .string()
+    .required()
+    .label('Bairro')
+    .test(
+        'test-name', 'Bairro não Pode conter Número',
+        function(value) {
+            const nameRegex = /^([a-z A-Z])+$/;
+            let isValidName = nameRegex.test(value);
+            if(!isValidName) {
+                return false;
+            }
+            return true;
+        }
+    ),
+    cidade: yup
+    .string()
+    .required()
+    .label('Cidade')
+    .test(
+        'test-name', 'Cidade não Pode conter Número',
+        function(value) {
+            const nameRegex = /^([a-z A-Z])+$/;
+            let isValidName = nameRegex.test(value);
+            if(!isValidName) {
+                return false;
+            }
+            return true;
+        }
+    ),
+    escola: yup
+    .string()
+    .required()
+    .label('Escola onde estuda ou estudou'),
+    curso: yup
+    .string()
+    .required()
+    .label('Curso'),
+    ano: yup
+    .number()
+    .required()
+    .label('Ano de formatura'),
+    semestre: yup
+    .number()
+    .required()
+    .label('Semestre de Formatura'),
+    mes: yup
+    .number()
+    .required()
+    .label('Mês de Formatura'),
+    periodo: yup
+    .number()
+    .required()
+    .label('Periodo/Ano/Ciclo/Módulo'),
+    horario: yup
+    .number()
+    .required()
+    .label('Horário de Estudo'),
 })
 
 export default class SignUp extends Component { 
@@ -115,7 +229,12 @@ export default class SignUp extends Component {
         <SafeAreaView style={styles.safe}>
         <Image style={styles.logo} source={logo}/>
         <Formik
-            initialValues={{ name: '', email: '', password: '', cpg: '', rg: '', sexo: '', orgao: '', ctps: '', pai: '', mae: '', civil: '', deficiencia: '' }}
+            initialValues={{ name: '', email: '', password: '', cpg: '', rg: '', 
+                sexo: '', orgao: '', ctps: '', pai: '', mae: '', civil: '', 
+                deficiencia: '', cep: '', logradouro:'', numero:'', complemento:'',
+                uf:'', bairro:'', cidade:'', telefone1:'', telefone2:'', escola:'',
+                curso:'', ano:'', semestre:'', mes:'', periodo:'', horario:'',
+            }}
             onSubmit={(values, actions) =>{
                 alert(JSON.stringify(values));
                 setTimeout(() => {
@@ -188,31 +307,6 @@ export default class SignUp extends Component {
                     <Text style={{ color: 'red' }}>{formikProps.errors.mae}</Text>                        
                     </View>
 
-{/* 
-
-                    <View style={styles.view}>
-                        <Text style={{marginBottom: 3}}>E-mail</Text>
-                        <TextInput placeholder="example@example.com" style={styles.input}
-                        onChangeText={formikProps.handleChange('email')}
-                        onBlur={formikProps.handleBlur('email')}
-                        />
-                        <Text style={styles.erro}>
-                        {formikProps.touched.email && formikProps.errors.email}
-                        </Text>
-                    </View>
-
-                    <View style={styles.view}>
-                        <Text style={{marginBottom: 3}}>Senha</Text>
-                        <TextInput  placeholder="Senha" style={styles.input}
-                        onChangeText={formikProps.handleChange('password')}
-                        onBlur={formikProps.handleBlur('password')}
-                        secureTextEntry
-                        />
-                        <Text style={styles.erro}>
-                        {formikProps.touched.password && formikProps.errors.password}
-                        </Text>
-                    </View> */}
-
                     <Text style={{marginBottom: 3, marginVertical: 5, marginHorizontal: 20}}>Sexo</Text>    
                     <View style={styles.drop}>
                         <Picker  selectedValue={this.state.sexo} 
@@ -252,6 +346,176 @@ export default class SignUp extends Component {
                             <Picker.Item style={styles.dropText} label= "Mútipla" value= "mutipla"/>
                         </Picker>
                     </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>CEP</Text>
+                    <TextInput style={styles.input} placeholder="CEP"
+                    onChangeText={formikProps.handleChange("cep")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.cep}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Logradouro</Text>
+                    <TextInput style={styles.input} placeholder="Logradouro"
+                    onChangeText={formikProps.handleChange("logradouro")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.logradouro}</Text>                        
+                    </View>
+                    
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Numero</Text>
+                    <TextInput style={styles.input} placeholder="Numero"
+                    onChangeText={formikProps.handleChange("numero")}
+                    keyboardType='numeric'
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.numero}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Complemento</Text>
+                    <TextInput style={styles.input} placeholder="Complemento"
+                    onChangeText={formikProps.handleChange("complemento")}
+                    />
+
+                    </View>
+                    
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>UF</Text>
+                    <TextInput style={styles.input} placeholder="UF"
+                    onChangeText={formikProps.handleChange("uf")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.uf}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Bairro</Text>
+                    <TextInput style={styles.input} placeholder="Bairro"
+                    onChangeText={formikProps.handleChange("bairro")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.bairro}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Cidade</Text>
+                    <TextInput style={styles.input} placeholder="Cidade"
+                    onChangeText={formikProps.handleChange("cidade")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.cidade}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Telefone 1</Text>
+                    <TextInput style={styles.input} placeholder="Telefone 1"
+                    onChangeText={formikProps.handleChange("telefone1")}
+                    keyboardType='numeric'
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.telefone1}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Telefone 2</Text>
+                    <TextInput style={styles.input} placeholder="Telefone 2"
+                    onChangeText={formikProps.handleChange("telefone2")}
+                    keyboardType='numeric'
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.telefone2}</Text>                        
+                    </View>
+                        
+
+                    
+                    <View style={styles.view}>
+                        <Text style={{marginBottom: 3}}>E-mail</Text>
+                        <TextInput placeholder="example@example.com" style={styles.input}
+                        onChangeText={formikProps.handleChange('email')}
+                        onBlur={formikProps.handleBlur('email')}
+                        />
+                        <Text style={styles.erro}>
+                        {formikProps.touched.email && formikProps.errors.email}
+                        </Text>
+                    </View>
+
+                    <View style={styles.view}>
+                        <Text style={{marginBottom: 3}}>Senha</Text>
+                        <TextInput  placeholder="Senha" style={styles.input}
+                        onChangeText={formikProps.handleChange('password')}
+                        onBlur={formikProps.handleBlur('password')}
+                        secureTextEntry
+                        />
+                        <Text style={styles.erro}>
+                        {formikProps.touched.password && formikProps.errors.password}
+                        </Text>
+                    </View>
+
+                    <View style={styles.view}>
+                        <Text style={{marginBottom: 3}}>Confirmar Senha</Text>
+                        <TextInput  placeholder="Confirmar Senha" style={styles.input}
+                        onChangeText={formikProps.handleChange('confirmPassword')}
+                        onBlur={formikProps.handleBlur('confirmPassword')}
+                        secureTextEntry
+                        />
+                        <Text style={styles.erro}>
+                        {formikProps.touched.confirmPassword && formikProps.errors.confirmPassword}
+                        </Text>
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Escola onde estuda ou estudou</Text>
+                    <TextInput style={styles.input} placeholder="Escola onde estuda ou estudou"
+                    onChangeText={formikProps.handleChange("escola")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.escola}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Curso</Text>
+                    <TextInput style={styles.input} placeholder="Curso"
+                    onChangeText={formikProps.handleChange("curso")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.curso}</Text>                        
+                    </View>
+                    
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Ano de formatura</Text>
+                    <TextInput style={styles.input} placeholder="Ano de formatura"
+                    onChangeText={formikProps.handleChange("ano")}
+                    keyboardType='numeric'
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.ano}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Semestre de formatura</Text>
+                    <TextInput style={styles.input} placeholder="Semestre de formatura"
+                    onChangeText={formikProps.handleChange("semestre")}
+                    />
+
+                    </View>
+                    
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Mês da formatura</Text>
+                    <TextInput style={styles.input} placeholder="Mês da formatura"
+                    onChangeText={formikProps.handleChange("mes")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.mes}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Periodo/Ano/Ciclo/Módulo</Text>
+                    <TextInput style={styles.input} placeholder="Periodo/Ano/Ciclo/Módulo"
+                    onChangeText={formikProps.handleChange("periodo")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.periodo}</Text>                        
+                    </View>
+
+                    <View style={styles.view}>
+                    <Text style={{ marginBottom: 3 }}>Horário de Estudo</Text>
+                    <TextInput style={styles.input} placeholder="Horário de Estudo"
+                    onChangeText={formikProps.handleChange("horario")}
+                    />
+                    <Text style={{ color: 'red' }}>{formikProps.errors.horario}</Text>                        
+                    </View>
+
 
 
 
